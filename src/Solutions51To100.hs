ffd19abd8ddad2af26b31919ff26031fb2292c62
James Ralston <jpralston1@gmail.com>
2015-04-14 17:46:23 -0400

    added solution to problem 55

--- src/Solutions51To100.hs
+++ src/Solutions51To100.hs
@@ -21,13 +21,18 @@ solutions51To100 n = do
 
 solutions51To100_ :: Int -> IO ()
 solutions51To100_ n 
+  | n == 55 = solution55
   | n == 58 = solution58
   | n == 92 = solution92
-  | n == 94 = solution94
   | n == 97 = solution97
   | n == 347 = solution347
   | otherwise = print "solution not present in program"
 
+
+solution55 = putStrLn . show . length $ filter lychrel [1..10000]
+  where lychrel x = not . any isPalindromic . take 50 . drop 1 $ iterate foo x
+          where foo x = x + x'
+                  where x' = read . reverse $ show x :: Integer
   
 solution92 = putStrLn . show . sum $ map solver [1..hi]
   where solver n 
@@ -49,21 +54,7 @@ solution97 = putStrLn . drop 2357197 . show $ 28433 * 2^7830457 + 1
 isInt :: (Integral a, RealFrac b) => b -> a -> Bool
 isInt x n = (round $ 10^(fromIntegral n)*(x-(fromIntegral $ round x)))==0
 
-solution94 = putStrLn . show . sum $ map func [2..333333333]
-  where func x 
-          | tbb = 6 * x
-          | tgb = 3 * x + 1
-          | tlb = 3 * x - 1
-          | otherwise = 0
-          where n = fromIntegral x
-                sp  = (3 * n ^ 2) - 1
-                tg2 = sqrt (sp - 2*n)
-                tl2 = sqrt (sp + 2*n)
-                tg = (n + 1) * tg2
-                tl = (n - 1) * tl2
-                tgb = isInt tg2 15
-                tlb = isInt tl2 15
-                tbb = and [tgb,tlb]
+
 
 solution347 = 
   putStrLn . show . 
